5ad24112850ae3d2054546ffa8d316791a4689b0
venkat143-v <81428675+venkat143-v@users.noreply.github.com>
2021-06-16 16:20:02 +0530

    Add files via upload

---
+++ Full_number.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Full_number
+{
+public static void main(String[] args)
+{
+Scanner obj = new Scanner(System.in);
+System.out.println("Enter number:");
+int num = obj.nextInt();//12345
+int n = num;//n=12345
+int f = 1,d;
+while(num!=0)
+{
+d=num%10;
+num = num/10;
+if(n%2==0 && d%2==1)
+{
+f=2;
+break;
+}
+else if(n%2==1 && d%2==0)
+{
+f=2;
+break;
+}
+}
+if(f==1 && n%2==0)
+System.out.println("Full even number");
+else if(f==1 && n%2==1)
+System.out.println("Full odd number");
+else
+System.out.println("Mixed number");
+}}
